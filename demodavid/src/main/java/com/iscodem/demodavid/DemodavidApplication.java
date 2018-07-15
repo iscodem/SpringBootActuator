08478c305d7bd47269ce440907d40d1526b6062f
David Porras Cordova <iscodem@gmail.com>
2018-07-14 20:03:02 -0500

    primera configuracion

---
+++ demodavid/src/main/java/com/iscodem/demodavid/DemodavidApplication.java
@@ -0,0 +1,12 @@
+package com.iscodem.demodavid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodavidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodavidApplication.class, args);
+	}
+}
---
+++ demodavid/src/main/java/com/iscodem/demodavid/model/Persona.java
@@ -0,0 +1,86 @@
+package com.iscodem.demodavid.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
+
+@Entity
+@Table(name = "tabla_persona") //Renombra la creación de la tabla en base de datos
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Persona {
+    @Id
+    @GeneratedValue()
+    private int id;
+    private String nombres;
+    private String apellidos;
+    private Date fecNac;
+    private int edad;
+    private String dni;
+
+    public Persona(int id, String nombres, String apellidos, Date fecNac, int edad, String dni) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.fecNac = fecNac;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public Persona() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Date getFecNac() {
+        return fecNac;
+    }
+
+    public void setFecNac(Date fecNac) {
+        this.fecNac = fecNac;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+}
+
