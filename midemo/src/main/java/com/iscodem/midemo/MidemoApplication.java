0a5d7e76c2f5115ed362e64b9102d4c3219aeda2
David Porras Cordova <iscodem@gmail.com>
2018-07-17 20:33:47 -0500

    Primera importacion

---
+++ midemo/src/main/java/com/iscodem/midemo/MidemoApplication.java
@@ -0,0 +1,12 @@
+package com.iscodem.midemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidemoApplication.class, args);
+	}
+}
